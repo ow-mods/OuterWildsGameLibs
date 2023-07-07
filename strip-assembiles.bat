703e8582e3c805c1fe88461ce125685c6a6fd5b9
_nebula <41904486+misternebula@users.noreply.github.com>
2023-07-07 12:28:58 +0100

    Revert "Improve code a lot"
    This reverts commit ef4244a4d68e4527f1fc3207db13bd30a4478f13.

--- strip-assembiles.bat
+++ strip-assembiles.bat
@@ -1,4 +1,5 @@
 @echo off 
+SETLOCAL ENABLEDELAYEDEXPANSION
 
 set toPublicize=Assembly-CSharp.dll Assembly-CSharp-firstpass.dll
 set dontTouch=Newtonsoft.Json.dll mscorlib.dll netstandard.dll System.Core.dll System.Data.dll System.dll
@@ -14,8 +15,11 @@ echo managedPath: %managedPath%
 
 set outPath=%~dp0\package\lib
 
-@REM Strip all assembiles, but keep them private.
-%~dp0\tools\NStrip.exe "%managedPath%" -o %outPath%
+@REM Iterate over every assembly, checking if we should strip or just copy
+(for %%a in ("%managedpath%\*") do (
+  set filename=%%~nxa
+  call :SUB !filename!
+))
 
 @REM Strip and publicize assemblies from toPublicize.
 (for %%a in (%toPublicize%) do (
@@ -24,11 +28,25 @@ set outPath=%~dp0\package\lib
   %~dp0\tools\NStrip.exe "%managedPath%\%%a" -o "%outPath%\%%a" -cg -p --cg-exclude-events
 ))
 
-@REM Copy over original assemblies for ones we don't want to touch.
-(for %%a in (%dontTouch%) do (
-  echo a: %%a
+pause
+goto :eof
 
-  xcopy "%managedPath%\%%a" "%outPath%\%%a" /y /v
+@REM Takes in a filename, and either strips it or copies it directly to the output folder.
+:SUB filename
+set "foundMatch=false"
+@REM Check file against all files in dontTouch
+(for %%b in (%dontTouch%) do (
+  set checkDll=%%b
+	
+  if "%1" == "!checkDll!" (
+    @REM Filename matches an entry in dontTouch, so just copy the dll
+    xcopy "%managedPath%\%1" "%outPath%\%1" /y /v
+	goto :eof
+  )
 ))
 
-pause
+if %foundMatch% == false (
+  @REM Filename doesn't match anything in dontTouch, so strip normally
+  %~dp0\tools\NStrip.exe "%managedPath%\%1" -o "%outPath%\%1"
+)
+goto :eof
