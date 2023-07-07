ef4244a4d68e4527f1fc3207db13bd30a4478f13
_nebula <41904486+misternebula@users.noreply.github.com>
2023-07-07 12:28:43 +0100

    Improve code a lot

--- strip-assembiles.bat
+++ strip-assembiles.bat
@@ -1,5 +1,4 @@
 @echo off 
-SETLOCAL ENABLEDELAYEDEXPANSION
 
 set toPublicize=Assembly-CSharp.dll Assembly-CSharp-firstpass.dll
 set dontTouch=Newtonsoft.Json.dll mscorlib.dll netstandard.dll System.Core.dll System.Data.dll System.dll
@@ -15,11 +14,8 @@ echo managedPath: %managedPath%
 
 set outPath=%~dp0\package\lib
 
-@REM Iterate over every assembly, checking if we should strip or just copy
-(for %%a in ("%managedpath%\*") do (
-  set filename=%%~nxa
-  call :SUB !filename!
-))
+@REM Strip all assembiles, but keep them private.
+%~dp0\tools\NStrip.exe "%managedPath%" -o %outPath%
 
 @REM Strip and publicize assemblies from toPublicize.
 (for %%a in (%toPublicize%) do (
@@ -28,25 +24,11 @@ set outPath=%~dp0\package\lib
   %~dp0\tools\NStrip.exe "%managedPath%\%%a" -o "%outPath%\%%a" -cg -p --cg-exclude-events
 ))
 
-pause
-goto :eof
+@REM Copy over original assemblies for ones we don't want to touch.
+(for %%a in (%dontTouch%) do (
+  echo a: %%a
 
-@REM Takes in a filename, and either strips it or copies it directly to the output folder.
-:SUB filename
-set "foundMatch=false"
-@REM Check file against all files in dontTouch
-(for %%b in (%dontTouch%) do (
-  set checkDll=%%b
-	
-  if "%1" == "!checkDll!" (
-    @REM Filename matches an entry in dontTouch, so just copy the dll
-    xcopy "%managedPath%\%1" "%outPath%\%1" /y /v
-	goto :eof
-  )
+  xcopy "%managedPath%\%%a" "%outPath%\%%a" /y /v
 ))
 
-if %foundMatch% == false (
-  @REM Filename doesn't match anything in dontTouch, so strip normally
-  %~dp0\tools\NStrip.exe "%managedPath%\%1" -o "%outPath%\%1"
-)
-goto :eof
+pause
